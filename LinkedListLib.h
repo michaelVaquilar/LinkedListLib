560304dce0dd98c43acab390cd09d38262c66619
tensign1444 <tensign1444@gmail.com>
2023-01-19 22:06:43 -0800

    added insert before and after to header file.

--- LinkedListLib.h
+++ LinkedListLib.h
@@ -2,6 +2,8 @@
 #ifndef LINKEDLIST_LINKEDLISTLIB_H
 #define LINKEDLIST_LINKEDLISTLIB_H
 
+#include <stdbool.h>
+
 typedef struct Node{
     void **value;
     struct Node *next;
@@ -21,5 +23,12 @@ void Add(LIST *list, void *value);
 
 void DestroyList(LIST *list);
 
+void DumpList(LIST *list);
+
+int IndexOf(LIST *list, void *value);
+
+bool InsertNodeBeforeTarget(LIST *list, void *TargetValue, void *newValue);
+
+bool InsertNodeAfterTarget(LIST *list, void *TargetValue, void *newValue);
 
 #endif //LINKEDLIST_LINKEDLISTLIB_H
