c155c0f1d87a1ccd6b922b449cb1bfde94ae072e
tensign1444 <tensign1444@gmail.com>
2023-01-19 22:05:13 -0800

    added insert before and after.

--- LinkedListLib.c
+++ LinkedListLib.c
@@ -9,15 +9,15 @@ bool initialized = false;
 
 /// Initializes our linked list so we have a memory location for it.
 /// Using calloc so that the memory is already set to 0 instead of empty.
-/// \return memory location
+/// \return memory location that we will have access too.
 LIST *Init(){
     initialized = true;
     return calloc(1, sizeof(LIST));
 }
 
-/// Adds a value to the end of the linked list.
+/// Adds a value to the linked list.
 /// \param value, value to be added
-/// \return none
+/// \return none, only used as a break.
 void Add(LIST *list, void *value) {
     NODE *pList = calloc(1, sizeof(NODE));
     pList->value = value;
@@ -37,7 +37,9 @@ void Add(LIST *list, void *value) {
 }
 
 
-///Destroys the linked list by setting head and tail = null;
+/// Destroys the list aka get's rid of the memory location
+/// by freeing the allocated memory.
+/// \param list, list to be destroyed
 void DestroyList(LIST *list){
     free(list);
 }
@@ -51,13 +53,65 @@ void DumpList(LIST *list){
         *curr = *curr->next;
     }
 }
+/// Finds the index of a specific value.
+/// \param list, the linked list to search.
+/// \param value, the value to look for.
+/// \return int, the index of the value.
 int IndexOf(LIST *list, void *value){
     NODE *curr = list->head;
     int counter = 1;
-    while(curr->value != value){
+    while(curr->value != value && curr != NULL){
         *curr = *curr->next;
         counter++;
     }
+    if(curr == NULL) {
+        return -1;
+    }
     return counter;
 }
+
+/// Inserts a node (aka value) before the target value the user is looking for.
+/// \param list, list to insert the new item into.
+/// \param TargetValue, value to insert new value at.
+/// \param newValue, new value to be inserted.
+/// \return true if the node was successfully added, false if the target value is not in the existing list.
+bool InsertNodeBeforeTarget(LIST *list, void *TargetValue, void *newValue){
+    int indexOfTarget = IndexOf(list, TargetValue);
+    if(indexOfTarget == -1){
+        return false;
+    }
+    NODE *curr = list->head;
+    NODE *temp = calloc(1, sizeof(NODE));
+    temp->value = newValue;
+    for(int i = 0; i < indexOfTarget - 1; i++){
+        *curr = *curr->next;
+    }
+    temp->next = curr;
+    *curr = *temp;
+    list->count++;
+    return true;
+}
+
+/// Inserts the new Node (aka value) after the specified target value.
+/// \param list, the list to insert the new node into.
+/// \param TargetValue, value to insert a new node after.
+/// \param newValue, the new value to insert.
+/// \return true if the node was successfully added, false if the target value is not in the existing list.
+bool InsertNodeAfterTarget(LIST *list, void *TargetValue, void *newValue){
+    int indexOfTarget = IndexOf(list, TargetValue);
+    if(indexOfTarget == -1){
+        return false;
+    }
+    NODE *curr = list->head;
+    NODE *temp = calloc(1, sizeof(NODE));
+    temp->value = newValue;
+    for(int i = 0; i < indexOfTarget; i++){
+        *curr = *curr->next;
+    }
+    temp->next = curr;
+    *curr = *temp;
+    list->count++;
+    return true;
+}
+
 
--- main.c
+++ main.c
@@ -5,13 +5,15 @@ int main() {
     int i = 10;
     int x = 20;
     int y = 30;
+    int b = 33;
     LIST *list = Init();
 
     Add(list, &i);
     Add(list, &x);
     Add(list, &y);
 
-    int b = IndexOf(list, &x);
-    printf("%i", b);
+    InsertNodeBeforeTarget(list,&i,&b);
+
+    printf("%d", *(list->head->value));
     return 0;
 }
